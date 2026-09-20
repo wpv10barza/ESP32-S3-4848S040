11cd7c4f778d09fa5a2820973b5e40e0dc106e0d
wpv10barza <polovaleriow@gmail.com>
2026-09-19 21:33:04 -0500

    Remove redundant compatibility/optional files

--- scripts/flash-panel.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-# Compatibility wrapper: physical flash/monitor is now BLOCK 6.
-cd "$(dirname "$0")/.."
-exec "$(pwd)/scripts/06_flash_monitor_guition.sh" "$@"
